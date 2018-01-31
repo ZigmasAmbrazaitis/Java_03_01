11149bc89f0dac7686da3a625d060922b78d0ec9
Zigmas <zambrazaitis@gmail.com>
2018-01-31 10:21:01 +0200

    uzduotis1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Iveskite skaiciu: ");
+        int sk = sc.nextInt();
+        if (sk%2 == 0) {
+            System.out.println("Skaicius lyginis.");
+        } else {
+            System.out.println("Skaicius nelyginis.");
+        }
+
     }
 }
